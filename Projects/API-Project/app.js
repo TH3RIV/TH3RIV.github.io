5ce939fd0616a976fdfd1cffa93e37b287f82132
TH3RIV <116128342+TH3RIV@users.noreply.github.com>
2022-12-06 20:17:10 +0000

    Updated code
    Code updated due to boredAPI being blocked for some security reason

--- Projects/API-Project/app.js
+++ Projects/API-Project/app.js
@@ -1,18 +1,3 @@
-// **************** ACTIVITY ****************
-
-const actDesc = document.querySelector('#actDesc');
-const randActBtn = document.querySelector('#actBtn');
-
-const randAct = async () => {
-    let act = await axios.get("http://www.boredapi.com/api/activity/");
-    actDesc.innerText = `"${act.data.activity}"`;
-}
-
-randActBtn.addEventListener('click', async function (e) {
-    e.preventDefault();
-    randAct();
-})
-
 // **************** MOVIE / SHOW ****************
 
 const msPoster = document.querySelector('#msPoster');
@@ -31,20 +16,16 @@ const randMS = async () => {
         let randID = Math.floor((Math.random() * latest.data.id) + 1);
         let tmdb = await axios.get(`https://api.themoviedb.org/3/movie/${randID}?api_key=587f7613fd27cf7a55e08375dbfd8ad6`);
         let imdbID = tmdb.data.imdb_id;
-        if (tmdb.data.success === false) {
-            randMS();
-        }
-        else {
-            msLink.href = `https://www.imdb.com/title/${imdbID}/`;
-            msPoster.src = `https://www.themoviedb.org/t/p/w600_and_h900_bestv2${tmdb.data.poster_path}`;
-            msTitle.innerText = `"${tmdb.data.title}"`;
-            msDesc.innerHTML = `<b>Summary:</b> "${tmdb.data.overview}"`;
-            msLang.innerHTML = `<b>Language:</b> ${tmdb.data.original_language}`;
-            msLength.innerHTML = `<b>Length:</b> ${tmdb.data.runtime} min.`;
-            msDate.innerHTML = `<b>Released:</b> ${tmdb.data.release_date}`;
-            msRating.innerHTML = `<b>TMDB Rating:</b> ${tmdb.data.vote_average}/10`;
-            console.log(randID);
-        }
+
+        msLink.href = `https://www.imdb.com/title/${imdbID}/`;
+        msPoster.src = `https://www.themoviedb.org/t/p/w600_and_h900_bestv2${tmdb.data.poster_path}`;
+        msTitle.innerText = `"${tmdb.data.title}"`;
+        msDesc.innerHTML = `<b>Summary:</b> "${tmdb.data.overview}"`;
+        msLang.innerHTML = `<b>Language:</b> ${tmdb.data.original_language}`;
+        msLength.innerHTML = `<b>Length:</b> ${tmdb.data.runtime} min.`;
+        msDate.innerHTML = `<b>Released:</b> ${tmdb.data.release_date}`;
+        msRating.innerHTML = `<b>TMDB Rating:</b> ${tmdb.data.vote_average}/10`;
+        console.log(randID);
     }
     catch (err) {
         msDesc.innerHTML = `Unexpected error, please try again.`;
@@ -59,5 +40,4 @@ msBtn.addEventListener('click', async function (e) {
 
 // **************** EXECUTE ON LOAD ****************
 
-randAct();
 randMS();
