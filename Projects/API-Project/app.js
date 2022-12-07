f2e46d6f709ad5ba9cf596676f04a299b1e447d9
TH3RIV <116128342+TH3RIV@users.noreply.github.com>
2022-12-07 23:51:14 +0000

    Update
    New project addition

--- Projects/API-Project/app.js
+++ Projects/API-Project/app.js
@@ -14,13 +14,13 @@ const randMS = async () => {
     let arr = ['movie', 'tv'];
 
     try {
-        let indexRand = Math.floor(Math.random() * arr.length);
-        let index = arr[indexRand];
-        let latest = await axios.get(`https://api.themoviedb.org/3/${index}/latest?api_key=587f7613fd27cf7a55e08375dbfd8ad6&language=en-US`);
+        let ctgIndex = Math.floor(Math.random() * arr.length);
+        let ctg = arr[ctgIndex];
+        let latest = await axios.get(`https://api.themoviedb.org/3/${ctg}/latest?api_key=587f7613fd27cf7a55e08375dbfd8ad6&language=en-US`);
         let randID = Math.floor((Math.random() * latest.data.id) + 1);
-        let tmdb = await axios.get(`https://api.themoviedb.org/3/${index}/${randID}?api_key=587f7613fd27cf7a55e08375dbfd8ad6&language=en-US`);
+        let tmdb = await axios.get(`https://api.themoviedb.org/3/${ctg}/${randID}?api_key=587f7613fd27cf7a55e08375dbfd8ad6&language=en-US`);
 
-        if (index === 'movie') {
+        if (ctg === 'movie') {
             msLink.href = `https://www.themoviedb.org/movie/${randID}-${tmdb.data.original_title}`;
             msPoster.src = `https://image.tmdb.org/t/p/w500${tmdb.data.poster_path}`;
             msTitle.innerText = `"${tmdb.data.title}"`;
@@ -29,9 +29,9 @@ const randMS = async () => {
             msLength.innerHTML = `<b>Length:</b> ${tmdb.data.runtime} min.`;
             msGenre.innerHTML = `<b>Genre:</b> ${tmdb.data.genres[0].name}`;
             msDate.innerHTML = `<b>Released:</b> ${tmdb.data.release_date}`;
-            console.log(`Index: ${index} and ID: ${randID}`);
+            console.log(`Category: ${ctg} and ID: ${randID}`);
         }
-        else if (index === 'tv') {
+        else if (ctg === 'tv') {
             msLink.href = `https://www.themoviedb.org/tv/${randID}-${tmdb.data.original_title}`;
             msPoster.src = `https://image.tmdb.org/t/p/w500${tmdb.data.poster_path}`;
             msTitle.innerText = `"${tmdb.data.original_name}"`;
@@ -40,7 +40,7 @@ const randMS = async () => {
             msLength.innerHTML = `<b>Length:</b> ${tmdb.data.episode_run_time} min.`;
             msGenre.innerHTML = `<b>Genre:</b> ${tmdb.data.genres[0].name}`;
             msDate.innerHTML = `<b>Released:</b> ${tmdb.data.first_air_date}`;
-            console.log(`Index: ${index} and ID: ${randID}`);
+            console.log(`Category: ${ctg} and ID: ${randID}`);
         }
 
     }
