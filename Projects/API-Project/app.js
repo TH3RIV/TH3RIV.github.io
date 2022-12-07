3ed58d15c8593ed7835fd37affe2ecf4123c8b17
TH3RIV <116128342+TH3RIV@users.noreply.github.com>
2022-12-07 11:16:48 +0000

    update
    Update to the code, seems like errors been minimised, yet can't do much about TMDB having no info on some titles.

--- Projects/API-Project/app.js
+++ Projects/API-Project/app.js
@@ -6,25 +6,25 @@ const msTitle = document.querySelector('#msTitle');
 const msLang = document.querySelector('#msLang');
 const msLength = document.querySelector('#msLength');
 const msDate = document.querySelector('#msDate');
-const msRating = document.querySelector('#msRating');
+const msGenre = document.querySelector('#msGenre');
 const msDesc = document.querySelector('#msDesc');
 const msBtn = document.querySelector('#msBtn')
 
 const randMS = async () => {
     try {
-        let latest = await axios.get('https://api.themoviedb.org/3/movie/latest?api_key=587f7613fd27cf7a55e08375dbfd8ad6');
+        let latest = await axios.get('https://api.themoviedb.org/3/movie/latest?api_key=587f7613fd27cf7a55e08375dbfd8ad6&language=en-US');
         let randID = Math.floor((Math.random() * latest.data.id) + 1);
-        let tmdb = await axios.get(`https://api.themoviedb.org/3/movie/${randID}?api_key=587f7613fd27cf7a55e08375dbfd8ad6`);
+        let tmdb = await axios.get(`https://api.themoviedb.org/3/movie/${randID}?api_key=587f7613fd27cf7a55e08375dbfd8ad6&language=en-US`);
         let imdbID = tmdb.data.imdb_id;
 
         msLink.href = `https://www.imdb.com/title/${imdbID}/`;
-        msPoster.src = `https://www.themoviedb.org/t/p/w600_and_h900_bestv2${tmdb.data.poster_path}`;
+        msPoster.src = `https://image.tmdb.org/t/p/w500${tmdb.data.poster_path}`;
         msTitle.innerText = `"${tmdb.data.title}"`;
         msDesc.innerHTML = `<b>Summary:</b> "${tmdb.data.overview}"`;
-        msLang.innerHTML = `<b>Language:</b> ${tmdb.data.original_language}`;
+        msLang.innerHTML = `<b>Language:</b> ${tmdb.data.spoken_languages[0].english_name}`;
         msLength.innerHTML = `<b>Length:</b> ${tmdb.data.runtime} min.`;
         msDate.innerHTML = `<b>Released:</b> ${tmdb.data.release_date}`;
-        msRating.innerHTML = `<b>TMDB Rating:</b> ${tmdb.data.vote_average}/10`;
+        msGenre.innerHTML = `<b>Genre:</b> ${tmdb.data.genres[0].name}`;
         console.log(randID);
     }
     catch (err) {
