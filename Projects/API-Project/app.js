7983e8b2776e64d8f068cd51086b10c606153d16
TH3RIV <116128342+TH3RIV@users.noreply.github.com>
2022-12-07 17:53:29 +0000

    update
    Had to rewrite quite a bit of JS to show the tv series as well.

--- Projects/API-Project/app.js
+++ Projects/API-Project/app.js
@@ -11,31 +11,42 @@ const msDesc = document.querySelector('#msDesc');
 const msBtn = document.querySelector('#msBtn');
 
 const randMS = async () => {
+    let arr = ['movie', 'tv'];
+    let indexRand = Math.floor(Math.random() * arr.length);
+    let index = arr[indexRand];
+
     try {
-        const arr = ['movie', 'tv'];
-        let indexRand = Math.floor(Math.random() * arr.length);
-        let index = arr[indexRand];
         let latest = await axios.get(`https://api.themoviedb.org/3/${index}/latest?api_key=587f7613fd27cf7a55e08375dbfd8ad6&language=en-US`);
-
         let randID = Math.floor((Math.random() * latest.data.id) + 1);
-        let tmdb = await axios.get(`https://api.themoviedb.org/3/movie/${randID}?api_key=587f7613fd27cf7a55e08375dbfd8ad6&language=en-US`);
-        let imdbID = tmdb.data.imdb_id;
+        let tmdb = await axios.get(`https://api.themoviedb.org/3/${index}/${randID}?api_key=587f7613fd27cf7a55e08375dbfd8ad6&language=en-US`);
 
-        if (tmdb.status == 200 && tmdb.data.adult === false) {
-            msLink.href = `https://www.imdb.com/title/${imdbID}/`;
+        if (index === 'movie') {
+            msLink.href = `https://www.themoviedb.org/movie/${randID}-${tmdb.data.original_title}`;
             msPoster.src = `https://image.tmdb.org/t/p/w500${tmdb.data.poster_path}`;
             msTitle.innerText = `"${tmdb.data.title}"`;
             msDesc.innerHTML = `<b>Summary:</b> "${tmdb.data.overview}"`;
             msLang.innerHTML = `<b>Language:</b> ${tmdb.data.spoken_languages[0].english_name}`;
             msLength.innerHTML = `<b>Length:</b> ${tmdb.data.runtime} min.`;
-            msDate.innerHTML = `<b>Released:</b> ${tmdb.data.release_date}`;
             msGenre.innerHTML = `<b>Genre:</b> ${tmdb.data.genres[0].name}`;
+            msDate.innerHTML = `<b>Released:</b> ${tmdb.data.release_date}`;
             console.log(`Index: ${index} and ID: ${randID}`);
         }
+        else if (index === 'tv') {
+            msLink.href = `https://www.themoviedb.org/tv/${randID}-${tmdb.data.original_title}`;
+            msPoster.src = `https://image.tmdb.org/t/p/w500${tmdb.data.poster_path}`;
+            msTitle.innerText = `"${tmdb.data.original_name}"`;
+            msDesc.innerHTML = `<b>Summary:</b> "${tmdb.data.overview}"`;
+            msLang.innerHTML = `<b>Language:</b> ${tmdb.data.spoken_languages[0].english_name}`;
+            msLength.innerHTML = `<b>Length:</b> ${tmdb.data.episode_run_time} min.`;
+            msGenre.innerHTML = `<b>Genre:</b> ${tmdb.data.genres[0].name}`;
+            msDate.innerHTML = `<b>Released:</b> ${tmdb.data.first_air_date}`;
+            console.log(`Index: ${index} and ID: ${randID}`);
+        }
+
     }
     catch (err) {
-        console.log(err);
         randMS();
+        console.log(err);
     }
 }
 
