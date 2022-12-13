126eaa3e56ff94892b3ef0938f7be92a1404e9d3
TH3RIV <116128342+TH3RIV@users.noreply.github.com>
2022-12-13 23:38:58 +0000

    Code update
    Updated the code and styles to use good methodology, now it sends the email and stays on the page! YAY!

--- scripting/contact.js
+++ scripting/contact.js
@@ -1,5 +1,7 @@
 // ************* OPEN / CLOSE CONTACT FORM *************
 
+const sent = document.querySelector('#sent');
+
 const contactLink = document.querySelector('#contactLink');
 contactLink.addEventListener('click', () => {
     document.querySelector("#contact-form").style.display = "block";
@@ -8,4 +10,23 @@ contactLink.addEventListener('click', () => {
 const btnClose = document.querySelector('#btnClose');
 btnClose.addEventListener('click', () => {
     document.querySelector("#contact-form").style.display = "none";
+    sent.innerText = "";
+});
+
+// ************* SEND INFORMATION *************
+
+const form = document.querySelector('#contactForm');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', form.action);
+    xhr.send(formData);
+
+    sent.innerText = "Message sent, I'll get back to you ASAP!";
+
+    form.reset();
 });
